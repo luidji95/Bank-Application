1f40d6ab2e75b755ccd3c82613f15a877b035e1a
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-01-22 00:41:08 +0100

    HomePage UI component

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import HomePage from "./HomePage";
 function App() {
   return (
     <div className="main">
-      <h1>Hello, World!</h1>
-      <p>This is a test.</p>
+      <HomePage></HomePage>
     </div>
   );
 }
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 const HomePage = () => {
-  return <h1>Bank Application</h1>;
+  return (
+    <>
+      <header>
+        <div className="logo">Horizon</div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#partners">Partners</a>
+            </li>
+            <li>
+              <a href="#features">Features</a>
+            </li>
+          </ul>
+        </nav>
+        <button className="cta">Log in</button>
+      </header>
+      <section className="hero">
+        <div className="hero-text">
+          <h1>Let some elegance into your finance</h1>
+          <p>
+            Simplify your financial journey with us. Join today and experience
+            hassle-free banking.
+          </p>
+          <button>Open an Account</button>
+        </div>
+        <div className="hero-image">
+          <img
+            src="/bankimages/bankapp-images/app.png"
+            alt="Phone"
+            className="phone"
+          />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default HomePage;
